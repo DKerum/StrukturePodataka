bece5eae950a73a602a5c9d9f79be852fe621166
DKerum <klk>
2025-10-14 01:31:27 +0200

    nedovrsen program

--- zad1/zad1.c
+++ zad1/zad1.c
@@ -1,9 +1,41 @@
+
 #define _CRT_NO_SECURE_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define max_string 50
 
 typedef struct student {
-
+	char ime[max_string];
+	char prezime[max_string];
+	int bodovi;
 }stud;
+
+//funkcija koja broji studente u datoteci
+
+int brojStudenata(){
+	FILE *dat = fopen("Studenti.txt", "r");
+	if(dat == NULL)
+		return -1;
+	int br = 0;
+	char buffer[150];
+	int c;
+	while((c = fgetc(dat)) != EOF) {
+		if(c == '\n') {
+			br++;
+		}
+	}
+	// Ako datoteka nije prazna i ne završava novim redom, povećaj broj za 1
+	/*fseek(file, -1, SEEK_END);
+	if(ftell(file) > 0 && fgetc(file) != '\n') {
+		br++;
+	}
+		br++;
+	*/
+	fclose(dat);
+	return br;
+}
 
 int main() 
 {
